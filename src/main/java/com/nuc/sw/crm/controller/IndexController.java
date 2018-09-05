7187bea60040c69c2049059876a951dd16ee6f23
gk <846558172@qq.com>
2018-09-05 19:08:57 +0800

    :green_heart: 修复 CI 构建

--- src/main/java/com/nuc/sw/crm/controller/IndexController.java
+++ src/main/java/com/nuc/sw/crm/controller/IndexController.java
@@ -1,15 +1,28 @@
 package com.nuc.sw.crm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.websocket.server.PathParam;
 
 @Controller
 
 public class IndexController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
         return "index";
     }
+
+    @RequestMapping("/service/{path}")
+    public String service(@PathVariable("path") String path) {
+        return "service/"+path;
+    }
+
+//    @RequestMapping("/{path}")
+//    public String path2Html(@PathVariable("path")String path){
+//        return path;
+//    }
 
 }
