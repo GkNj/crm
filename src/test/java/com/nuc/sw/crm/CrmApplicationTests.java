93927f162a3df2ea22ea801e0fb5b18bd3e1853f
youngxhui <youngxhui@gmail.com>
2018-09-05 18:51:43 +0800

    :green_heart: 修复 CI 构建

--- src/test/java/com/nuc/sw/crm/CrmApplicationTests.java
+++ src/test/java/com/nuc/sw/crm/CrmApplicationTests.java
@@ -2,15 +2,21 @@ package com.nuc.sw.crm;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class CrmApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
