e3257aa600f6f081dc29b639f8ec7f7f0661db88
yangbingyu <1594581933@qq.com>
2018-09-07 19:12:36 +0800

    更新客户接口

--- src/main/java/com/nuc/sw/crm/repository/CustomerRepository.java
+++ src/main/java/com/nuc/sw/crm/repository/CustomerRepository.java
@@ -2,6 +2,9 @@ package com.nuc.sw.crm.repository;
 
 import com.nuc.sw.crm.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +13,9 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 
     List<Customer> findCustomersByUId(int uId);
+
+    @Modifying
+    @Query(value = "update customer set c_address =?1,c_class =?2,c_credit =?3,c_satisfaction =?4,c_email =?5,c_post =?6,c_website =?7 where c_id =?8",nativeQuery = true)
+    void updateCustomerByCId(String address,String cClass,int credit,int satisfaction,String email,String post,String website,int id);
+
 }
