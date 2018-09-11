76f82678a8a045ad07bd19d1317c96fd5fb0fe73
yangbingyu <1594581933@qq.com>
2018-09-11 21:26:27 +0800

    :ok_hand: 更新客户信息管理部分

--- src/main/java/com/nuc/sw/crm/service/serviceImpl/CustomerServiceImpl.java
+++ src/main/java/com/nuc/sw/crm/service/serviceImpl/CustomerServiceImpl.java
@@ -1,18 +1,28 @@
 package com.nuc.sw.crm.service.serviceImpl;
 
-import com.nuc.sw.crm.entity.Customer;
-import com.nuc.sw.crm.repository.CustomerRepository;
+import com.nuc.sw.crm.entity.*;
+import com.nuc.sw.crm.repository.*;
 import com.nuc.sw.crm.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private LinkmanRepository linkmanRepository;
+    @Autowired
+    private TradeRepository tradeRepository;
+    @Autowired
+    private OrdersRepository ordersRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public List<Customer> queryCustomerByUid(int uId) {
@@ -22,6 +32,8 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Customer queryCustomerByCid(int cId) {
+
+
         return customerRepository.getOne(cId);
     }
 
@@ -30,5 +42,106 @@ public class CustomerServiceImpl implements CustomerService {
 
     }
 
+    @Override
+    public List<Linkman> queryLinkmenByCid(int cId) {
 
+        return linkmanRepository.findLinkmenByCId(cId);
+    }
+
+    @Override
+    public List<Trade> queryTradesByCid(int cId) {
+
+        return tradeRepository.findTradesByCId(cId);
+    }
+
+    @Override
+    public List<Orders> queryOrdersByCid(int cId) {
+
+        return ordersRepository.findOrdersByCId(cId);
+    }
+
+    @Override
+    public int countOrdersByCId(int cId) {
+
+        return ordersRepository.countOrdersByCId(cId);
+    }
+
+    @Override
+    public int countTradesByCId(int cId) {
+
+        return tradeRepository.countTradesByCId(cId);
+    }
+
+    @Override
+    public int countLinkmenByCId(int cId) {
+
+        return linkmanRepository.countLinkmenByCId(cId);
+    }
+
+    @Override
+    public Linkman addLinkman(Linkman linkman) {
+
+        return linkmanRepository.save(linkman);
+    }
+
+    @Override
+    public Trade addTrade(Trade trade) {
+
+        return tradeRepository.save(trade);
+    }
+
+    @Override
+    public void deleteLinkman(int lId) {
+
+        linkmanRepository.deleteById(lId);
+    }
+
+    @Override
+    public void deleteTrade(int tId) {
+
+        tradeRepository.deleteById(tId);
+    }
+
+    @Override
+    public Orders queryOrdersByOId(int oId) {
+
+        return ordersRepository.queryOrdersByOId(oId);
+    }
+
+    @Override
+    public Customer updateCustomer(Customer customer) {
+
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Linkman updateLinkman(Linkman linkman) {
+
+        return linkmanRepository.save(linkman);
+    }
+
+    @Override
+    public Trade updateTrade(Trade trade) {
+
+        return tradeRepository.save(trade);
+    }
+
+    @Override
+    public List<Product> queryProduct(int oId) {
+
+        return productRepository.findProductsByOId(oId);
+    }
+
+//    @Override
+//    public Orders savePrice(Orders orders) {
+//
+//        return ordersRepository.save(orders);
+//    }
+
+
+    @Override
+    public int updatePrice(double price, int oid) {
+
+        return ordersRepository.updatePrice(price,oid);
+    }
 }
