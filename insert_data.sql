5227d70db43131090ca9af8c462d9917be52055d
youngxhui <youngxhui@gmail.com>
2018-09-12 10:02:40 +0800

    :card_file_box: 执行数据库相关的变化 - 更新数据库表插入数据文件

--- insert_data.sql
+++ insert_data.sql
@@ -3,12 +3,12 @@
 INSERT INTO crm.user (id, name, password, position, sex, username) VALUES (79, '123', '$2a$10$yVsPxxB3QEZYnoHlK83ojuL2ic0kzkp.jCwpqwedpyhrO6Kl5OzsO', 'ROOT', '女', '1713010101');
 INSERT INTO crm.user (id, name, password, position, sex, username) VALUES (32, 'Root', '$2a$10$1Mh01qeGRtBSHX0ERnRspOipzy4FFT0z.fPkG2ogIQwTGXnIL1bHu', 'ROOT', '男', 'root');
 -- role 数据
-INSERT INTO crm.role (id, permission, name, introduction) VALUES (1, 1, 'ROOT', '超级管理员');
-INSERT INTO crm.role (id, permission, name, introduction) VALUES (2, 2, 'ADMIN', '系统管理');
-INSERT INTO crm.role (id, permission, name, introduction) VALUES (3, 3, 'AM', '客户经理');
-INSERT INTO crm.role (id, permission, name, introduction) VALUES (4, 4, 'SA', '销售主管');
-INSERT INTO crm.role (id, permission, name, introduction) VALUES (5, 5, 'SM', '高管');
-INSERT INTO crm.role (id, permission, name, introduction) VALUES (0, 0, 'NULL', '为止职位');
+INSERT INTO crm.role (id, name, introduction) VALUES (1, 'ROOT', '超级管理员');
+INSERT INTO crm.role (id, name, introduction) VALUES (2, 'ADMIN', '系统管理');
+INSERT INTO crm.role (id, name, introduction) VALUES (3, 'AM', '客户经理');
+INSERT INTO crm.role (id, name, introduction) VALUES (4, 'SA', '销售主管');
+INSERT INTO crm.role (id, name, introduction) VALUES (5, 'SM', '高管');
+INSERT INTO crm.role (id, name, introduction) VALUES (0, 'NULL', '为止职位');
 
 -- user_role 表
 INSERT INTO crm.user_role (id, ur_role_id, ur_user_id) VALUES (80, 1, 79);
